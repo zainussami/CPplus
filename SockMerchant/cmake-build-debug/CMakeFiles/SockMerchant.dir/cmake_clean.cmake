2c1b4a97875bb7906bd0351eeb94e96091b13d88
Zain Ansari <zainussami@gmail.com>
2020-07-14 22:27:40 -0400

    Added Sock Merchant

---
+++ SockMerchant/cmake-build-debug/CMakeFiles/SockMerchant.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SockMerchant.dir/main.cpp.o"
+  "SockMerchant.pdb"
+  "SockMerchant"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SockMerchant.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
